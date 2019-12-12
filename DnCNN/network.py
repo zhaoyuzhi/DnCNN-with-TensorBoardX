df8b9c5c5950c14bf25e215c85436fee5ad20fb7
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2019-12-12 20:01:33 +0800

    Add files via upload

---
+++ DnCNN/network.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+from network_module import *
+
+# ----------------------------------------
+#         Initialize the networks
+# ----------------------------------------
+def weights_init(net, init_type = 'normal', init_gain = 0.02):
+    """Initialize network weights.
+    Parameters:
+        net (network)   -- network to be initialized
+        init_type (str) -- the name of an initialization method: normal | xavier | kaiming | orthogonal
+        init_gain (float)    -- scaling factor for normal, xavier and orthogonal
+    In our paper, we choose the default setting: zero mean Gaussian distribution with a standard deviation of 0.02
+    """
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and classname.find('Conv') != -1:
+            if init_type == 'normal':
+                torch.nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                torch.nn.init.xavier_normal_(m.weight.data, gain = init_gain)
+            elif init_type == 'kaiming':
+                torch.nn.init.kaiming_normal_(m.weight.data, a = 0, mode = 'fan_in')
+            elif init_type == 'orthogonal':
+                torch.nn.init.orthogonal_(m.weight.data, gain = init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+        elif classname.find('BatchNorm2d') != -1:
+            torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+            torch.nn.init.constant_(m.bias.data, 0.0)
+
+    # apply the initialization function <init_func>
+    print('initialize network with %s type' % init_type)
+    net.apply(init_func)
+
+# ----------------------------------------
+#         Denoising CNN (DnCNN)
+# ----------------------------------------
+class DnCNN(nn.Module):
+    def __init__(self, opt):
+        super(DnCNN, self).__init__()
+        layers = []
+        layers.append(Conv2dLayer(opt.in_channels, opt.start_channels, 3, 1, 1, pad_type = opt.pad, activation = opt.activ, norm = 'none'))
+        layers.append(nn.ReLU(inplace=True))
+        for _ in range(opt.m_block - 2):
+            layers.append(Conv2dLayer(opt.start_channels, opt.start_channels, 3, 1, 1, pad_type = opt.pad, activation = opt.activ, norm = opt.norm))
+        layers.append(Conv2dLayer(opt.in_channels, opt.start_channels, 3, 1, 1, pad_type = opt.pad, activation = 'sigmoid', norm = 'none'))
+        self.dncnn = nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.dncnn(x)
+        return out
---
+++ DnCNN/train.py
@@ -0,0 +1,66 @@
+import argparse
+import os
+
+import trainer
+
+if __name__ == "__main__":
+    # ----------------------------------------
+    #        Initialize the parameters
+    # ----------------------------------------
+    parser = argparse.ArgumentParser()
+    # Pre-train, saving, and loading parameters
+    parser.add_argument('--pre_train', type = bool, default = True, help = 'pre-train ot not')
+    parser.add_argument('--save_mode', type = str, default = 'epoch', help = 'saving mode, and by_epoch saving is recommended')
+    parser.add_argument('--save_by_epoch', type = int, default = 10, help = 'interval between model checkpoints (by epochs)')
+    parser.add_argument('--save_by_iter', type = int, default = 1000, help = 'interval between model checkpoints (by iterations)')
+    parser.add_argument('--load_name', type = str, default = '', help = 'load the pre-trained model with certain epoch')
+    # GPU parameters
+    parser.add_argument('--multi_gpu', type = bool, default = False, help = 'True for more than 1 GPU')
+    parser.add_argument('--gpu_ids', type = str, default = '0, 1, 2, 3', help = 'gpu_ids: e.g. 0  0,1  0,1,2  use -1 for CPU')
+    parser.add_argument('--cudnn_benchmark', type = bool, default = True, help = 'True for unchanged input data type')
+    # Training parameters
+    parser.add_argument('--epochs', type = int, default = 50, help = 'number of epochs of training that ensures 100K training iterations')
+    parser.add_argument('--batch_size', type = int, default = 128, help = 'size of the batches, 128 is recommended')
+    parser.add_argument('--lr', type = float, default = 0.001, help = 'Adam: learning rate')
+    parser.add_argument('--b1', type = float, default = 0.9, help = 'Adam: decay of first order momentum of gradient')
+    parser.add_argument('--b2', type = float, default = 0.999, help = 'Adam: decay of second order momentum of gradient')
+    parser.add_argument('--weight_decay', type = float, default = 0, help = 'weight decay for optimizer')
+    parser.add_argument('--epoch_decreased', type = int, default = 30, help = 'the certain epoch that lr decreased')
+    parser.add_argument('--lr_decreased', type = float, default = 0.0001, help = 'decreased learning rate at certain epoch')
+    parser.add_argument('--num_workers', type = int, default = 4, help = 'number of cpu threads to use during batch generation')
+    # Initialization parameters
+    parser.add_argument('--pad', type = str, default = 'reflect', help = 'pad type of networks')
+    parser.add_argument('--activ', type = str, default = 'relu', help = 'activation type of networks')
+    parser.add_argument('--norm', type = str, default = 'bn', help = 'normalization type of networks')
+    parser.add_argument('--in_channels', type = int, default = 3, help = 'input channels for generator')
+    parser.add_argument('--out_channels', type = int, default = 3, help = 'output channels for generator')
+    parser.add_argument('--start_channels', type = int, default = 64, help = 'start channels for generator')
+    parser.add_argument('--m_block', type = int, default = 17, help = 'number of convolutional blocks in DnCNN, 17 or 20')
+    parser.add_argument('--init_type', type = str, default = 'kaiming', help = 'initialization type of generator')
+    parser.add_argument('--init_gain', type = float, default = 0.02, help = 'initialization gain of generator')
+    # Dataset parameters
+    parser.add_argument('--baseroot', type = str, default = 'C:\\Users\\yzzha\\Desktop\\dataset\\DIV2K\\DIV2K_train_HR', help = 'images baseroot')
+    parser.add_argument('--crop_size', type = int, default = 256, help = 'single patch size')
+    parser.add_argument('--geometry_aug', type = bool, default = False, help = 'geometry augmentation (scaling)')
+    parser.add_argument('--angle_aug', type = bool, default = True, help = 'geometry augmentation (rotation, flipping)')
+    parser.add_argument('--scale_min', type = float, default = 1, help = 'min scaling factor')
+    parser.add_argument('--scale_max', type = float, default = 1, help = 'max scaling factor')
+    parser.add_argument('--mu', type = int, default = 0, help = 'Gaussian noise mean')
+    parser.add_argument('--sigma', type = int, default = 25, help = 'Gaussian noise variance')
+    parser.add_argument('--bit', type = int, default = 8, help = 'the bitwidth of training images. 8 represents [0, 255]; 16 represents [0, 65535]')
+    opt = parser.parse_args()
+
+    # ----------------------------------------
+    #        Choose CUDA visible devices
+    # ----------------------------------------
+    if opt.multi_gpu == True:
+        os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu_ids
+        print('Multi-GPU mode, %s GPUs are used' % (opt.gpu_ids))
+    else:
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+        print('Single-GPU mode')
+    
+    # ----------------------------------------
+    #                 Trainer
+    # ----------------------------------------
+    trainer.Trainer(opt)
