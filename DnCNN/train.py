fd2b11a9907f8d0c992beb4628049c029a822665
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2019-12-18 10:14:10 +0800

    Update train.py

--- DnCNN/train.py
+++ DnCNN/train.py
@@ -45,6 +45,8 @@ if __name__ == "__main__":
     parser.add_argument('--save_root', type = str, default = './model', help = 'models saving root')
     parser.add_argument('--sample_root', type = str, default = './sample', help = 'sample images root')
     parser.add_argument('--crop_size', type = int, default = 256, help = 'single patch size')
+    # QuadBayer parameters
+    parser.add_argument('--short_expo_per_pattern', type = int, default = 2, help = 'QuadBayer short exposure number')
     parser.add_argument('--color_bias_aug', type = bool, default = False, help = 'max scaling factor')
     parser.add_argument('--color_bias_level', type = bool, default = 0.0, help = 'max scaling factor')
     parser.add_argument('--noise_aug', type = bool, default = True, help = 'max scaling factor')
